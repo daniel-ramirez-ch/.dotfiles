c7850c2404e61d9a8a77f7128158318e941beda8
Daniel Ramirez <hi@danielrachi.com>
2024-01-02 17:24:14 -0500

    Add nvim-tree

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -226,6 +226,19 @@ require('lazy').setup({
       require("bufferline").setup {}
     end
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        filters = {dotfiles = true},
+      }
+    end,
+  }
 }, {})
 
 -- [[ Setting options ]]
@@ -283,8 +296,11 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- nvim-tree mappings
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>', {desc = 'Toggle nvim-tree'})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
