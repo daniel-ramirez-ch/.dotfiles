df66766cee5aa9ae14f5141bb14c5696863e611d
Daniel Ramirez <hi@danielrachi.com>
2024-01-02 17:23:59 -0500

    Add bufferline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -220,6 +220,12 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  {
+    'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function ()
+      require("bufferline").setup {}
+    end
+  },
 }, {})
 
 -- [[ Setting options ]]
