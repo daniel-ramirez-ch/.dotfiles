f9bb4ed87a5fc683c29a8b3f312a6d1d60e7f43c
Daniel Ramirez <hi@danielrachi.com>
2024-01-08 11:27:26 -0500

    Show hidden files on search

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -207,19 +207,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
   {
     'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
     config = function ()
@@ -235,7 +222,6 @@ require('lazy').setup({
     },
     config = function()
       require("nvim-tree").setup {
-        filters = {dotfiles = true},
         actions = {
           open_file = {
             quit_on_open = true,
@@ -340,6 +326,11 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
 }
 
 -- Enable telescope fzf native, if installed
